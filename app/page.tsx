eb6f5bff9efd4334320a3a49f667c242003f8ce3
H3DZZZ <86843403+H3DZZZ@users.noreply.github.com>
2024-11-19 13:25:32 +0200

    setup fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold">Welcome to Next.js</h1>
+      <h1 className="text-3xl font-bold">Welcome to Next.js (Inter)</h1>
+      <h1 className="font-space-grotesk text-3xl font-bold">
+        Welcome to Next.js (Space Grotesk)
+      </h1>
     </main>
   );
 }

68aec3f80591172889561d7cab8dbbd69e454aa9
H3DZZZ <86843403+H3DZZZ@users.noreply.github.com>
2024-12-03 12:55:51 +0200

    Mobile nav, and navLinks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,8 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 export default async function Home() {
   const session = await auth();
 
   console.log(session);
-  return (
-    <main>
-      <h1 className="text-3xl font-bold">Welcome to Next.js (Inter)</h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">LogOut</Button>
-      </form>
-    </main>
-  );
+  return <main></main>;
 }
--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import MobileNavigation from "./mobile-navigation";
 import Theme from "./theme";
 
 const Navbar = () => {
@@ -22,6 +23,7 @@ const Navbar = () => {
 
       <div className="flex-between gap-5">
         <Theme />
+        <MobileNavigation />
       </div>
     </nav>
   );

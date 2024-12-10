3ec2470d92d8a01d7712369e129c4f5d00f81386
H3DZZZ <86843403+H3DZZZ@users.noreply.github.com>
2024-12-10 15:08:50 +0200

    implement Question Card

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,3 +12,27 @@ export const getDeviconClassName = (techName: string) => {
 
   return `${techMap[normalizedTechName]} colored` || "devicon-devicon-plain";
 };
+
+export const getTimeStamp = (date: Date): string => {
+  const units: [string, number][] = [
+    ["year", 31536000],
+    ["month", 2592000],
+    ["week", 604800],
+    ["day", 86400],
+    ["hour", 3600],
+    ["minute", 60],
+    ["second", 1],
+  ];
+
+  const now = new Date();
+  const diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+
+  for (const [unit, secondsInUnit] of units) {
+    const value = Math.floor(diffInSeconds / secondsInUnit);
+    if (value >= 1) {
+      return `${value} ${unit}${value > 1 ? "s" : ""} ago`;
+    }
+  }
+
+  return "just now";
+};

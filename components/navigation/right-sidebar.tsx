9723ea5daec4ee3ba68fba4e0fc30192021161f1
H3DZZZ <86843403+H3DZZZ@users.noreply.github.com>
2025-03-27 16:02:33 +0200

    Show popular tags

--- components/navigation/right-sidebar.tsx
+++ components/navigation/right-sidebar.tsx
@@ -7,9 +7,15 @@ import ROUTES from "@/constants/routes";
 import TagCard from "../cards/tag-card";
 import { getHotQuestions } from "@/lib/actions/question.action";
 import DataRenderer from "../data-renderer";
+import { getTopTags } from "@/lib/actions/tag.action";
 
 const RightSidebar = async () => {
   const { success, data: hotQuestions, error } = await getHotQuestions();
+  const {
+    success: tagSuccess,
+    data: topTags,
+    error: tagError,
+  } = await getTopTags();
   return (
     <section className="custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
@@ -50,21 +56,32 @@ const RightSidebar = async () => {
         </div>
       </div>
 
-      {/* <div className="mt-16">
+      <div className="mt-16">
         <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
-        <div className="mt-7 flex flex-col gap-4">
-          {popularTags.map(({ _id, name, questions }) => (
-            <TagCard
-              key={_id}
-              _id={_id}
-              name={name}
-              questions={questions}
-              showCount
-              compact
-            />
-          ))}
-        </div>
-      </div> */}
+        <DataRenderer
+          data={topTags}
+          empty={{
+            title: "No tags found.",
+            message: "No tags have been created yet.",
+          }}
+          success={tagSuccess}
+          error={tagError}
+          render={(topTags) => (
+            <div className="mt-7 flex flex-col gap-4">
+              {topTags.map(({ _id, name, questions }) => (
+                <TagCard
+                  key={_id}
+                  _id={_id}
+                  name={name}
+                  questions={questions}
+                  showCount
+                  compact
+                />
+              ))}
+            </div>
+          )}
+        />
+      </div>
     </section>
   );
 };
--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -19,6 +19,7 @@ import {
   GetTagQuestionsSchema,
   PaginatedSearchParamsSchema,
 } from "../validations";
+import { dbConnect } from "../mongoose";
 
 export const getTags = async (
   params: PaginatedSearchParams
@@ -140,6 +141,21 @@ export const getTagQuestions = async (
         questions: JSON.parse(JSON.stringify(questions)),
         isNext,
       },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+};
+
+export const getTopTags = async (): Promise<ActionResponse<Tag[]>> => {
+  try {
+    await dbConnect();
+
+    const tags = await Tags.find().sort({ questions: -1 }).limit(5);
+
+    return {
+      success: true,
+      data: JSON.parse(JSON.stringify(tags)),
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;

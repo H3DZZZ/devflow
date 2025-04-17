5d4a58ac1bec8fd69b32f77b7339a1dd7721d3ab
H3DZZZ <86843403+H3DZZZ@users.noreply.github.com>
2025-04-17 15:40:37 +0300

    Implement recommendation questions based on interactions and tags

--- components/navigation/left-sidebar.tsx
+++ components/navigation/left-sidebar.tsx
@@ -30,7 +30,7 @@ const LeftSidebar = async () => {
               type="submit"
               className="base-medium w-fit !bg-transparent px-4 py-3"
             >
-              <LogOut className="size-5 text-black" />
+              <LogOut className="size-5 dark:text-white text-black" />
               <span className="text-dark300_light900 max-lg:hidden">
                 Logout
               </span>
